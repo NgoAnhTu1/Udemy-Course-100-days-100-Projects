169ad4493886b47dc237b03651a6eb2f1a8fd2a9
NgoAnhTu1 <ngoanhtu01626734727@gmail.com>
2025-05-18 21:19:56 +0700

    Day 2

---
+++ Day_2_Personalized Greeting Program.py
@@ -0,0 +1,8 @@
+name = input("What is yout namae?")
+age = int(input("How old are you?"))
+color = input("What is your favorite color?")
+
+
+print("\n---- Personalized Greeting ---")
+print(f"Hello, {name}!")
+print("You are {} years old and {} color is a beautiful color!".format(age, color))
