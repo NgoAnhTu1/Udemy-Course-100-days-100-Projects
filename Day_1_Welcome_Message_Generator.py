6921dc86dc56a351bdd6ead4ba0ef383c1c441c4
NgoAnhTu1 <ngoanhtu01626734727@gmail.com>
2025-05-18 21:18:23 +0700

    Day 1

---
+++ Day_1_Welcome_Message_Generator.py
@@ -0,0 +1,6 @@
+name = input('What is your name?')
+hobby = input('What is your favorite hobby?')
+
+print('\n--- Welcome Message ---')
+print(f'Hello, {name}!')
+print(f'Your favorite hobby is {hobby}')
